72f019c491a2bb2ffab040c71b7b3fd773753aa8
Davi Nascimento <68043001+davi41229@users.noreply.github.com>
2023-01-06 23:59:14 -0300

    Update ControllerDefault.py
    atualização com upload  de imagem.

--- aplication/controllers/ControllerDefault.py
+++ aplication/controllers/ControllerDefault.py
@@ -1,6 +1,10 @@
 from aplication import aplication
 from flask import render_template, redirect, request, url_for
 from flask_login import login_user, logout_user
+import base64
+from PIL import Image
+
+from werkzeug.security import generate_password_hash
 
 from werkzeug.security import generate_password_hash
 
@@ -20,9 +24,10 @@ def register():
     if request.method == 'POST': 
         name = request.form['name'] 
         email = request.form['email'] 
+        image = request.form['image'].encode()
         pwd = request.form['password'] 
 
-        info = Info(name, email, pwd)
+        info = Info(name, email, image, pwd)
         db.session.add(info) #REGISTRANDO O USUARIO NA SESSAO
         db.session.commit() #SALVANDO  OS DADOS NO BANCO
 
@@ -77,7 +82,12 @@ def editar(id):
     editar_usuario = Info.query.get(id)
     if request.method == 'POST': 
         editar_usuario.name = request.form['name'] 
+<<<<<<< HEAD
+        editar_usuario.email = request.form['email']
+        editar_usuario.image = request.form['image'].encode() 
+=======
         editar_usuario.email = request.form['email'] 
+>>>>>>> 646dc19a238e18ea42f47b84d802a8ce934cb245
         editar_usuario.password = generate_password_hash(request.form['password'])
         
         db.session.commit() #SALVANDO  OS DADOS NO BANCO
@@ -85,3 +95,13 @@ def editar(id):
         return redirect(url_for('contas'))
     return render_template( 'editar.html', editar_usuario=editar_usuario)
 
+
+@aplication.route('/save',methods=['GET', 'POST'])
+def salvar_imagem():
+    if request.method == 'POST':
+        image = request.form['image']
+        filename = image
+        img = Image.open(image)
+        rgb_img = img.convert('RGB')
+        rgb_img.save("uploads/" + filename + ".jpg")            
+    return render_template( 'upload.html')
